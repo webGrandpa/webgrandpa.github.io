64dbfc978cb344a2605b6843d86ff0a5ff922517
webGrandpa <goga.khilo@gmail.com>
2025-11-28 03:10:14 +0400

    Fix: Add .nojekyll to enforce correct MIME types

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  
+  base: '/', 
+  
   build: {
     outDir: 'docs',
     emptyOutDir: true,
